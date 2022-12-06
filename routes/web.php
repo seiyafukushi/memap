1e26c4ad4dc56f8b4479167019a4adea23d003fc
seiyafukushi <seiyaf294@gmail.com>
2022-12-07 06:12:42 +0900

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AlbumController;  //外部にあるAlbumControllerクラスをインポート。
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/', [AlbumController::class, 'index']);
